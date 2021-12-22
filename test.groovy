fa8b615cc7c27281d0467f33d8ea24093bb1a559
Chau Phan <chau.phan@of-client-825.fritz.box>
2021-12-22 23:25:29 +0100

    [MTP-2] Test Jira

--- test.groovy
+++ test.groovy
@@ -1,3 +1,65 @@
-def task = "python3 detect-jira-issue-keys.py".execute()
-task.waitFor()
-println task.text
+pipeline {
+    agent any
+    
+    stages {
+        stage('Git checkout') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/ptchau2003/DancingQueen.git']]])
+            }
+        }
+        stage('Build') {
+            steps {
+                echo 'Building..'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Deploy') {
+            
+            steps {
+                echo 'Deploying....'
+            }
+
+            steps {
+                when {
+                // Only say hello if a "greeting" is requested
+                    expression { pissueKeys != '' }
+                }
+                echo 'Testing condition'
+
+            }
+            post {
+                always {
+                    dir('.') {
+                        script {
+                            environmentType = "${ENV}"
+                            if (environmentType == "preprod") {
+                                environmentType = "unmapped"
+                            }
+        
+                            issueKeys = sh(
+                                returnStdout: true,
+                                script: './detect-jira-issue-keys.py'
+                            ).trim().split('\n') as List
+                        }
+                    }
+                   
+                 
+                            jiraSendDeploymentInfo site: 'chauphan.atlassian.net',
+                                environmentId: "${ENV}",
+                                environmentName: "${ENV}",
+                                environmentType: "${environmentType}",
+                                issueKeys: issueKeys
+                }
+                
+
+                cleanup {
+                    cleanWs()
+                }
+            }
+        }
+    }
+}
